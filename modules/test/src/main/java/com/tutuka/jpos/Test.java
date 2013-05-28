d297d1295a1bb47a54b5599471c25272bfb4fdfc
Tom Van Schoor <tom@tutuka.com>
2013-05-28 19:47:14 +0200

    First custom test module working

--- modules/test/src/main/java/com/tutuka/jpos/Test.java
+++ modules/test/src/main/java/com/tutuka/jpos/Test.java
@@ -1,5 +1,26 @@
 package com.tutuka.jpos;
 
-public class Test {
+import java.io.*;
+import org.jpos.iso.*;
+import org.jpos.util.*;
+import org.jpos.iso.channel.*;
+import org.jpos.iso.packager.*;
 
+public class Test implements ISORequestListener {
+	public Test() {
+		super();
+	}
+
+	public boolean process(ISOSource source, ISOMsg m) {
+		try {
+			m.setResponseMTI();
+			m.set(39, "00");
+			source.send(m);
+		} catch (ISOException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
 }
